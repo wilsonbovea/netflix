a45de3876bc2e1713039fb34b881c1c80f2970a9
Wilson Bovea Cifuentes <wilsonboveacifuentes@MacBook-Air-de-Wilson.local>
2024-06-17 17:42:59 +0200

    modification header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
 const Header = () => {
   return (
     <header>
-      <img
-        src="https://lereacteur-react-netflix.netlify.app/assets/logo-a79c3484.png"
-        alt=""
-      />
+      <img src="/logo.png" alt="" />
     </header>
   );
 };
